b224bd6a0e3f8a5858ff435a5cfdcdbfb5d10772
Ceroniko <ikomarisiy@gmail.com>
2020-12-25 16:07:08 +0200

    work with node-sass package for support .scss file

--- src/layouts/base/view.jsx
+++ src/layouts/base/view.jsx
@@ -1,12 +1,13 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Layout } from "antd";
+import "./style.scss";
 
 const View = React.memo(({ children }) => {
   return (
-    <Layout>
-      <Layout.Header>Header</Layout.Header>
-      <Layout.Content>{children}</Layout.Content>
-      <Layout.Footer>Footer</Layout.Footer>
+    <Layout className={"layout layout--base"}>
+      <Layout.Header className={"layout__header"}>Header</Layout.Header>
+      <Layout.Content className={"layout__content"}>{children}</Layout.Content>
+      <Layout.Footer className={"layout__footer"}>Footer</Layout.Footer>
     </Layout>
   );
 });
