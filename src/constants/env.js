e85fc3ec007b2c173f4de02f515b4414978ea861
Ceroniko <ikomarisiy@gmail.com>
2020-12-25 12:27:38 +0200

    add constants and assets folder

---
+++ src/constants/env.js
@@ -0,0 +1,12 @@
+/** Build environment */
+export const BUILD_ENV = process.env.NODE_ENV;
+export const IS_DEVELOPMENT = BUILD_ENV === "development";
+export const IS_PRODUCTION = BUILD_ENV === "production";
+
+/** Permissions */
+export const ALLOW_DEV_CONSOLE =
+  process.env.REACT_APP_ALLOW_DEV_CONSOLE === "true";
+export const ALLOW_REDUX_DEVTOOLS_EXTENSION =
+  process.env.REACT_APP_ALLOW_REDUX_DEVTOOLS_EXTENSION === "true";
+export const ALLOW_WINDOW_EXTENDS =
+  process.env.REACT_APP_ALLOW_WINDOW_EXTENDS === "true";
