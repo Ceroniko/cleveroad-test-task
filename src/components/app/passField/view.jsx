3522e4bc1b4614afd4f9b587bb0577673277fcaf
Ceroniko <ikomarisiy@gmail.com>
2020-12-26 12:06:25 +0200

    add password field component

---
+++ src/components/app/passField/view.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Form, Input } from "antd";
+import { LockOutlined } from "@ant-design/icons";
+
+const View = ({ input, meta }) => {
+  return (
+    <Form.Item
+      name="password"
+      required
+      help="Введите любой корректный пароль"
+      rules={[
+        {
+          required: true,
+          message: "Требуется заполнить поле пароля",
+        },
+        {
+          min: 8,
+          message: "Пароль должен содержать не менее 8 символов",
+        },
+        {
+          pattern: /^[A-Za-z0-9.-_]+$/i,
+          message: "Некорректный формат пароля",
+        },
+      ]}
+    >
+      <Input.Password
+        {...input}
+        prefix={<LockOutlined />}
+        placeholder="Пароль"
+      />
+    </Form.Item>
+  );
+};
+
+export { View };
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Root } from "./app/root";
 export { Header } from "./app/header";
 export { Navlink } from "./app/navlink";
 export { EmailField } from "./app/emailField";
+export { PassField } from "./app/passField";
